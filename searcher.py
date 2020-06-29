469ae45d6331f23084852cbf0873b5c562bcbb16
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2020-06-29 09:56:52 +0900

    refactored searcher to cache instanced CalcNode

--- searcher.py
+++ searcher.py
@@ -3,11 +3,19 @@ import heapq
 from abc import ABCMeta, abstractmethod
 from dataclasses import dataclass, field
 from data import Node
-from typing import List, Optional, Any
+from typing import List, Dict, Tuple, Optional, Any, ClassVar
 
 
 @dataclass
 class CalcNode:
+    cache: ClassVar[Dict[Tuple[int, int], 'CalcNode']] = {}
+
+    @classmethod
+    def new(cls, node: Node):
+        if node.as_tuple() not in cls.cache:
+            cls.cache[node.as_tuple()] = CalcNode(node)
+        return cls.cache[node.as_tuple()]
+
     node: Node
     fs: float = 0.0
     gs: float = 0.0
@@ -25,7 +33,7 @@ class CalcNode:
     @property
     def nexts(self) -> List['CalcNode']:
         if self._nexts is None:
-            self._nexts = [CalcNode(edge.get_opposite_node(self.node)) for edge in self.node.edges]
+            self._nexts = [CalcNode.new(edge.get_opposite_node(self.node)) for edge in self.node.edges]
         return self._nexts
 
     def __lt__(self, other: Any) -> bool:
@@ -44,51 +52,54 @@ class Searcher(metaclass=ABCMeta):
     def __init__(self, start: Node, goal: Node):
         self.start = start
         self.goal = goal
-        self.open_list: List[CalcNode] = []
-        self.close_list: List[CalcNode] = []
-        heapq.heapify(self.open_list)
 
     def calculate(self) -> List[Node]:
-        start = CalcNode(self.start)
-        start.fs = self._hs(start)
-        heapq.heappush(self.open_list, start)
         target: CalcNode
 
+        open_list: List[CalcNode] = []
+        close_list: List[CalcNode] = []
+        heapq.heapify(open_list)
+
+        start = CalcNode.new(self.start)
+        start.fs = self._hs(start)
+
+        heapq.heappush(open_list, start)
+
         while True:
-            if len(self.open_list) == 0:
+            if len(open_list) == 0:
                 print('No route found')
                 return []
 
-            target = heapq.heappop(self.open_list)
+            target = heapq.heappop(open_list)
             if target.node == self.goal:
                 break
 
-            self.close_list.append(target)
+            close_list.append(target)
 
             target.gs = target.fs - self._hs(target)
 
             for candidate in target.nexts:
                 fs = target.gs + self._cost(target, candidate) + self._hs(candidate)
 
-                opened_candidate = self._find_candidate(self.open_list, candidate)
+                opened_candidate = self._find_candidate(open_list, candidate)
                 if opened_candidate is not None:
                     if fs < opened_candidate.fs:
-                        self.open_list.remove(opened_candidate)
+                        open_list.remove(opened_candidate)
                         opened_candidate.fs = fs
                         opened_candidate.parent = target
-                        heapq.heappush(self.open_list, opened_candidate)
+                        heapq.heappush(open_list, opened_candidate)
                 else:
-                    closed_candidate = self._find_candidate(self.close_list, candidate)
+                    closed_candidate = self._find_candidate(close_list, candidate)
                     if closed_candidate is not None:
                         if fs < closed_candidate.fs:
-                            self.close_list.remove(closed_candidate)
+                            close_list.remove(closed_candidate)
                             closed_candidate.fs = fs
                             closed_candidate.parent = target
-                            heapq.heappush(self.open_list, closed_candidate)
+                            heapq.heappush(open_list, closed_candidate)
                     else:
                         candidate.fs = fs
                         candidate.parent = target
-                        heapq.heappush(self.open_list, candidate)
+                        heapq.heappush(open_list, candidate)
 
         return [li.node for li in reversed(list(target))]
 
@@ -97,8 +108,8 @@ class Searcher(metaclass=ABCMeta):
 
     def _find_candidate(self, li: List[CalcNode], candidate: CalcNode) -> Optional[CalcNode]:
         try:
-            return next(filter(lambda e: e.x == candidate.x and e.y == candidate.y, li))
-        except StopIteration:
+            return li[li.index(candidate)]
+        except ValueError:
             return None
 
     @abstractmethod
