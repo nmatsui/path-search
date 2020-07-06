e066cabc34f25dc8cfe02866c8bcb82183771c3f
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2020-07-06 14:52:16 +0900

    refactored main.py to extract the 'prepare_graph' function

--- main.py
+++ main.py
@@ -13,41 +13,51 @@ if len(sys.argv) != 2:
     print(f'usage: {sys.argv[0]} module_name')
     sys.exit(1)
 
-graph = import_module(sys.argv[1])
-SIZE: Tuple[int, int] = graph.SIZE  # type: ignore
-NODES: Dict[str, Node] = graph.NODES  # type: ignore
-EDGES: List[Edge] = graph.EDGES  # type: ignore
-START_NODE: Node = graph.START_NODE  # type: ignore
-GOAL_NODE: Node = graph.GOAL_NODE  # type: ignore
-
-
-for node in NODES.values():
-    node.edges = [edge for edge in EDGES if edge.st == node or edge.ed == node]
-
-
 GRAPH_NAME = 'graph.jpg'
 ASTAR_PATH_NAME = 'astar_path.jpg'
 DIJKSTRA_PATH_NAME = 'dijkstra_path.jpg'
 FAST_ASTAR_NAME = 'fast_astar_path.jpg'
 
 
+def prepare_graph(module_name: str) -> Tuple[Tuple[int, int], Dict[str, Node], List[Edge], Node, Node]:
+    graph = import_module(sys.argv[1])
+    size: Tuple[int, int] = graph.SIZE  # type: ignore
+    nodes: Dict[str, Node] = graph.NODES  # type: ignore
+    edges: List[Edge] = graph.EDGES  # type: ignore
+    start_node: Node = graph.START_NODE  # type: ignore
+    goal_node: Node = graph.GOAL_NODE  # type: ignore
+
+    for node in nodes.values():
+        node.edges = [edge for edge in edges if edge.st == node or edge.ed == node]
+
+    return size, nodes, edges, start_node, goal_node
+
+
 def main() -> None:
-    print('start')
+    print('## start ##')
 
+    print('prepare graph')
+    size, nodes, edges, start_node, goal_node = prepare_graph(sys.argv[1])
+
+    print('start searching by Astar algorithm')
     a1s = time.time()
-    astar_path = Astar(START_NODE, GOAL_NODE).calculate()
-    print(f'save astar path to {ASTAR_PATH_NAME}, time={time.time() - a1s}')
-    images.draw_path(ASTAR_PATH_NAME, SIZE, NODES, EDGES, astar_path)
+    astar_path = Astar(start_node, goal_node).calculate()
+    print(f'finish astar searching and save searched path as {ASTAR_PATH_NAME}, time={time.time() - a1s}')
+    images.draw_path(ASTAR_PATH_NAME, size, nodes, edges, astar_path)
 
+    print('start searching by Dijkstra algorithm')
     d1s = time.time()
-    dijkstra_path = Dijkstra(START_NODE, GOAL_NODE).calculate()
-    print(f'save dijkstra path to {DIJKSTRA_PATH_NAME}, time={time.time() - d1s}')
-    images.draw_path(DIJKSTRA_PATH_NAME, SIZE, NODES, EDGES, dijkstra_path)
+    dijkstra_path = Dijkstra(start_node, goal_node).calculate()
+    print(f'finish dijkstra searching and save searched path as {DIJKSTRA_PATH_NAME}, time={time.time() - d1s}')
+    images.draw_path(DIJKSTRA_PATH_NAME, size, nodes, edges, dijkstra_path)
 
+    print('start searching by Fast Astar algorithm')
     a2s = time.time()
-    astar_path = Astar2(START_NODE, GOAL_NODE).calculate()
-    print(f'save fast astar path to {FAST_ASTAR_NAME}, time={time.time() - a2s}')
-    images.draw_path(FAST_ASTAR_NAME, SIZE, NODES, EDGES, astar_path)
+    fast_astar_path = Astar2(start_node, goal_node).calculate()
+    print(f'finish searching and save searched path as {FAST_ASTAR_NAME}, time={time.time() - a2s}')
+    images.draw_path(FAST_ASTAR_NAME, size, nodes, edges, fast_astar_path)
+
+    print('## end ##')
 
 
 if __name__ == '__main__':
